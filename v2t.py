e0cefb37bffff53963a344de9a28fe326bfc83a5
Eric Guimaraes de Sousa Silva <eric.silva@paygo.com.br>
2020-10-09 21:24:44 -0300

    First dummy version

--- v2t.py
+++ v2t.py
@@ -2,12 +2,30 @@
 
 # Dependencies
 import speech_recognition as sr
+import os 
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
 
 #########
 # Input #
 #########
 
-input_file = ""
+input_file = "audio.wav"
+
+##############
+# Processing #
+##############
+
+# Recognizer
+r = sr.Recognizer()
+
+# Convertion
+with sr.AudioFile(input_file) as input:
+  # Listen
+  audio_data = r.record(input)
+  # Recognize
+  text = r.recognize_google(audio_data, language="pt-BR")
+  print(text)
 
 ##########
 # Output #
